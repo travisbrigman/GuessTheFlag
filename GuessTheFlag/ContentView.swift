3f634947b187a410faf967afba39c9afdc027b15
travisbrigman <travis1000@gmail.com>
2021-03-02 12:49:35 -0600

    feat: added accessibility labels to flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -18,6 +18,19 @@ struct Title: ViewModifier {
 }
 
 struct FlagImage: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
     var number: Int
     var countries: [String]
@@ -28,11 +41,15 @@ struct FlagImage: View {
             .clipShape(Capsule())
             .overlay(Capsule().stroke(Color.gray, lineWidth: 1))
             .shadow(color: .black, radius: 2)
+            .accessibility(label: Text(self.labels[self.countries[number], default: "unknown flag"]))
     }
     
 }
 
 struct ContentView: View {
+    
+
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy","Nigeria","Poland","Russia","Spain","UK","US"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
